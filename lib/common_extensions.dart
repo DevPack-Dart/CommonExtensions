175e3e3709a0a9759fad05532fc97ca77445f3ce
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-10-24 00:39:46 +0900

    add: lib root

---
+++ lib/common_extensions.dart
@@ -0,0 +1,14 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library common_extensions;
+
+export 'src/exts/number_extensions.dart';
+export 'src/exts/bool_extensions.dart';
+export 'src/exts/string_extensions.dart';
+export 'src/exts/function_extensions.dart';
+export 'src/exts/collection_extensions.dart';
+export 'src/exts/table_extensions.dart';
+export 'src/exts/result_extensions.dart';
+export 'src/exts/option_extensions.dart';
+export 'src/exts/union_extensions.dart';
---
+++ lib/common_libs.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library common_libs;
+
+export 'src/libpack/serializable_extensions.dart';
